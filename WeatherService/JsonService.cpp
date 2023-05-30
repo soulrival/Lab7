f6ea6d5e0b165b67412c768bf67a19c639124f0d
soulrival <zhdanov.artyom231003@gmail.com>
2023-05-30 10:48:28 +0300

    Json main

--- WeatherService/JsonService.cpp
+++ WeatherService/JsonService.cpp
@@ -1,5 +1,6 @@
 #include "JsonService.h"
 
+
 Weather JsonService::getWeather(string s)
 {
     ifstream fin(s);
@@ -7,12 +8,17 @@ Weather JsonService::getWeather(string s)
 
     json j;
     j = json::parse(fin);
-    string city = j["name"]; //  Киров
-    double lon = j["coord"]["lon"]; // 49.6601
-    double lat = j["coord"]["lat"]; // 58.5966  
-    double temperature = j["main"]["temp"]; // 5.69
-    string weather = j["weather"]["main"]; // дождь
-    double windSpeed = j["wind"]["speed"]; // 4.27
-    int clouds = j["clouds"]["all"]; // 100
+
+    string city = j["name"].get<string>(); //  Киров
+
+    double lon = j["coord"]["lon"].get<double>(); // 49.6601
+    double lat = j["coord"]["lat"].get<double>(); // 58.5966  
+    double temperature = j["main"]["temp"].get<double>(); // 5.69
+
+    string weather = j["weather"].get<vector<json>>()[0]["main"].get<string>(); // дождь
+
+    double windSpeed = j["wind"]["speed"].get<double>(); // 4.27
+    int clouds = j["clouds"]["all"].get<int>(); // 100
+
     return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
 }
